47b73fd3b3ac488d2c888f8179919914e318705a
Haytam Zanid <34218324+zHaytam@users.noreply.github.com>
2018-05-09 12:56:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,18 +12,18 @@ def read(*parts):
         return f.read()
 
 setup(
-  name = 'vaderSentiment',
+  name = 'vaderSentiment2',
   #packages = ['vaderSentiment'], # this must be the same as the name above
   packages = find_packages(exclude=['tests*']), # a better way to do it than the line above -- this way no typo/transpo errors
   include_package_data=True,
-  version = '3.2.1',
+  version = '3.2.1.1',
   description = 'VADER Sentiment Analysis. VADER (Valence Aware Dictionary and sEntiment Reasoner) is a lexicon and rule-based sentiment analysis tool that is specifically attuned to sentiments expressed in social media, and works well on texts from other domains.',
   long_description = read("README.rst"),
   long_description_content_type = 'text/markdown',
   author = 'C.J. Hutto',
   author_email = 'cjhutto@gatech.edu',
   license = 'MIT License: http://opensource.org/licenses/MIT',
-  url = 'https://github.com/cjhutto/vaderSentiment', # use the URL to the github repo
+  url = 'https://github.com/zHaytam/vaderSentiment', # use the URL to the github repo
   download_url = 'https://github.com/cjhutto/vaderSentiment/archive/master.zip', 
   keywords = ['vader', 'sentiment', 'analysis', 'opinion', 'mining', 'nlp', 'text', 'data', 
               'text analysis', 'opinion analysis', 'sentiment analysis', 'text mining', 'twitter sentiment',
